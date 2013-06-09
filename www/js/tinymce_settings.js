0b6330905712f8cf3d1e95ee321feec84c333510
michieljoris <michieljoris@gmail.com>
2013-06-09 13:36:16 +1000

    laptop update

--- www/js/tinymce_settings.js
+++ www/js/tinymce_settings.js
@@ -15,13 +15,15 @@ tinymce.init({
     toolbar: "save insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons", 
     save_enablewhendirty: true,
     style_formats: [
-        {title: 'Bold text', inline: 'b'},
-        {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
-        {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
-        {title: 'Example 1', inline: 'span', classes: 'example1'},
-        {title: 'Example 2', inline: 'span', classes: 'example2'},
-        {title: 'Table styles'},
-        {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+        // {title: 'Bold text', inline: 'b'},
+        // {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+        {title: 'Header 1', block: 'h1', styles: {}},
+        {title: 'Header 2', block: 'h2', styles: {}},
+        {title: 'Header 3', block: 'h3', styles: {}},
+        {title: 'Header 4', block: 'h4', styles: {}},
+        {title: 'Header 5', block: 'h5', styles: {}},
+        {title: 'Burnt-orange', inline: 'span', styles: {color: '#f26500'}}
+        // {title: 'Example 2', inline: 'span', classes: 'example2'}
     ]
 }); 
 
