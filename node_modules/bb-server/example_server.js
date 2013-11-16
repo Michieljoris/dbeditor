ab35585135c93baf48082d3edd1b5e4ee9950004
michieljoris <michieljoris@gmail.com>
2013-11-17 00:31:14 +1000

    sync

--- node_modules/bb-server/example_server.js
+++ node_modules/bb-server/example_server.js
@@ -3,7 +3,7 @@
 /*jshint maxparams:7 maxcomplexity:7 maxlen:150 devel:true newcap:false*/ 
 
 var server = require('./lib/bb-server.js'),
-    testMail = require("./testSendMail"),
+    // testMail = require("./testSendMail"),
     testGet = require("./testGet")
 ;
 
@@ -20,13 +20,21 @@ var options = {
     ,"silent": false
     ,"port": 7090
     ,postHandlers: {
-        "/sendmail" : testMail
+        // "/sendmail" : testMail
         }
     
     ,getHandlers: {
         "/testget" : testGet,
         "/testGet" : testGet
         }
+    ,sessions: {
+        expires: 30
+        // ,store: 'mysql'
+        ,store: 'memory'
+        // ,storeOpts: {
+        //     //options for mysql, memory doesn't need any
+        // }
+    }
 };
 
 server.go(options);
