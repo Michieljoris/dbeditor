4eb6b442cbb89c542b1923f1caa1d70cf91bd643
michieljoris <michieljoris@gmail.com>
2013-11-16 12:28:59 +1000

    syncing to linode now

---
+++ sync-to-linode.sh
@@ -0,0 +1,10 @@
+rsync --verbose  --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude "*bak" --exclude "*~" --exclude ".git" ~/www/sites/dbeditor/ michieljoris@linode:~/www/dbeditor/
+
+
+
+
+
+
+
+
+
